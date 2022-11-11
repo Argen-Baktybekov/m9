596889f85ae7efae943869cb093cbe1ca263c3d7
Argen Baktybekov <baktybekov.argen96@gmail.com>
2022-11-11 17:30:57 +0600

    HW-65 task1, add test data

---
+++ src/main/resources/db/migration/V202211111715__add_test_data.sql
@@ -0,0 +1,8 @@
+insert into item (category, description, image, name, price, qty)
+VALUES ('flowers', 'flowers for You', 'image1.jpeg', 'Rose', 15, 1);
+
+insert into item (category, description, image, name, price, qty)
+VALUES ('flowers', 'flowers for You', 'image2.jpeg', 'Tulpan', 10, 3);
+
+insert into item (category, description, image, name, price, qty)
+VALUES ('flowers', 'flowers for You', 'image3.jpeg', 'Astra', 5, 3);

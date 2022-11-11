d3fcdcbf0507ad4f73ffd3c2e3ced5d9e257228a
Argen Baktybekov <baktybekov.argen96@gmail.com>
2022-11-11 13:01:44 +0600

    start project

---
+++ src/main/java/edu/attractor/kg/m9/M9Application.java
@@ -0,0 +1,13 @@
+package edu.attractor.kg.m9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M9Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/attractor/kg/m9/M9ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.attractor.kg.m9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

a9642b259040064a9393438c89710aabf014db92
Argen Baktybekov <baktybekov.argen96@gmail.com>
2022-11-18 18:19:23 +0600

    fixed exception handler

--- src/main/java/edu/attractor/kg/m9/utils/ControllerExceptionHandler.java
+++ src/main/java/edu/attractor/kg/m9/utils/ControllerExceptionHandler.java
@@ -1,31 +1,23 @@
 package edu.attractor.kg.m9.utils;
 
 import edu.attractor.kg.m9.controllers.CustomerControllers;
-import edu.attractor.kg.m9.entities.Customer;
-import edu.attractor.kg.m9.entities.Item;
 import edu.attractor.kg.m9.exeptions.CustomerNotValidException;
 import edu.attractor.kg.m9.exeptions.ResourceNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice(basePackageClasses = {CustomerControllers.class})
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(value = {ResourceNotFoundException.class})
-//    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public String resourceNotFoundException(ResourceNotFoundException e){
 
-        return e.getMessage();
+    public ResponseEntity<String> resourceNotFoundException(ResourceNotFoundException e){
+
+        return ResponseEntity.ok(e.getMessage());
     }
 
     @ExceptionHandler(value = {CustomerNotValidException.class})
-//    @ResponseStatus(value = HttpStatus.OK)
     public ResponseEntity<String> customerNotValidException(CustomerNotValidException e){
 
         return ResponseEntity.ok(e.getMessage());
